61f337294f9f36167b43022293843344f041983f
Rahim Ali <139705342+rahim1976@users.noreply.github.com>
2025-02-06 02:03:22 +0500

    push

--- src/sanity/lib/client.ts
+++ src/sanity/lib/client.ts
@@ -1,11 +1,9 @@
-import { createClient } from "@sanity/client";
-
-import {projectId, dataset, apiVersion, token} from "../env"
+import { createClient } from 'next-sanity';
 
 export const client = createClient({
-  projectId, 
-  dataset, 
-  apiVersion, 
-  token,
-  useCdn: true,
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
+  apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2023-05-03',
+  useCdn: process.env.NODE_ENV === 'production',
+  token: process.env.SANITY_API_READ_TOKEN,
 });
